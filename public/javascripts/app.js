080f087d11e8fb54842a392e10494eab2b929916
Josh <joshcooper537@gmail.com>
2017-11-01 23:34:30 +0000

    backend

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,8 +2,27 @@
 
 var app = angular.module('app', []);
 
+app.factory('userList', userList);
 app.controller('mainCtrl', mainCtrl);
 
-function mainCtrl($scope) {
+function userList($http) {
+	var API_ROOT = 'getusers';
+	return {
+		get: function() {
+			return $http
+			.get(API_ROOT)
+			.then(function (resp) {
+				return resp.data;
+			})
+		}
+	};
+}
+
+function mainCtrl($scope, userList) {
+	$scope.pages = [];
 	
+	userList.get()
+	.then(function (data) {
+		$scope.pages = data;
+	});
 }
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-var users = [
-	{
-		name: 
-	}
-];
+
+// id, name, profileImg, bgColor (6 digit hexadecimal value), interests
+var users = [];
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
   res.sendFile('index.html', { root: 'public' });
+});
+
+router.get('/getusers', function(req, res) {
+	// return a JSON object list of users
+	res.send(users);
+});
+
+router.post('/getusers', function(req, res) {
+	// req object will have form: {name(string), profileImg(string URL), bgColor(string), interests(string) }
+	var newUser = req.body;
+	newUser.id = users.length;
+	users.push(newUser);
+	res.end('{"success" : "Updated Successfully", "status" : 200}');
 });
 
 module.exports = router;
