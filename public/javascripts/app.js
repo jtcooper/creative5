41424cccc09198b5c9fa9e5a722dd9b73e1e5b51
Josh <joshcooper537@gmail.com>
2017-11-02 00:02:13 +0000

    stuff

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,26 @@
 // javacript file for app
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.router']);
 
 app.factory('userList', userList);
 app.controller('mainCtrl', mainCtrl);
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+	    $stateProvider
+	    .state('home', {
+	        url: '/home',
+	        templateUrl: '/home.html',
+	        controller: 'mainCtrl' })   
+	    .state('user', {
+		    url: '/user/{id}',
+		    templateUrl: '/user.html',
+		    controller: 'pageCtrl'
+	    });
+	    $urlRouterProvider.otherwise('home');
+	}
+])
 
 function userList($http) {
 	var API_ROOT = 'getusers';
