4b4c7b102965cd61382510d358dfab56d7174f0e
Josh <joshcooper537@gmail.com>
2017-11-02 00:20:02 +0000

    stuff

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,42 +4,51 @@ var app = angular.module('app', ['ui.router']);
 
 app.factory('userList', userList);
 app.controller('mainCtrl', mainCtrl);
-app.config([
-    '$stateProvider',
-    '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-	    $stateProvider
-	    .state('home', {
-	        url: '/home',
-	        templateUrl: '/home.html',
-	        controller: 'mainCtrl' })   
-	    .state('user', {
-		    url: '/user/{id}',
-		    templateUrl: '/user.html',
-		    controller: 'pageCtrl'
-	    });
-	    $urlRouterProvider.otherwise('home');
-	}
-])
+// app.controller('pageCtrl', pageCtrl);
+// app.config([
+//     '$stateProvider',
+//     '$urlRouterProvider',
+//     function($stateProvider, $urlRouterProvider) {
+// 	    $stateProvider
+// 	    .state('home', {
+// 	        url: '/home',
+// 	        templateUrl: '/home.html',
+// 	        controller: 'mainCtrl' })   
+// 	    .state('user', {
+// 		    url: '/user/{id}',
+// 		    templateUrl: '/user.html',
+// 		    controller: 'pageCtrl'
+// 	    });
+// 	    $urlRouterProvider.otherwise('home');
+// 	}
+// ])
 
 function userList($http) {
 	var API_ROOT = 'getusers';
+	console.log("in userList");
 	return {
 		get: function() {
 			return $http
 			.get(API_ROOT)
 			.then(function (resp) {
+				console.log(resp.data);
 				return resp.data;
 			})
 		}
 	};
 }
-
 function mainCtrl($scope, userList) {
-	$scope.pages = [];
+	$scope.users = [];
+	console.log("in mainCtrl");
 	
 	userList.get()
 	.then(function (data) {
-		$scope.pages = data;
+		$scope.users = data;
 	});
+	$scope.createUser = function() {
+		//
+	}
 }
+// function pageCtrl($scope, userList) {
+// 	//
+// }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 
-// id, name, profileImg, bgColor (6 digit hexadecimal value), interests
-var users = [];
+// name, profileImg, bgColor (6 digit hexadecimal value), interests
+var users = [{name: "Bob", profileImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGgfEcu0eBLQZBhs_COrSRa_F4uixbP0dRCB33_o245ig6tLhb", bgColor: "FF0000", interests: "likes bananas"}];
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
@@ -12,13 +12,13 @@ router.get('/', function(req, res, next) {
 
 router.get('/getusers', function(req, res) {
 	// return a JSON object list of users
+	console.log(users);
 	res.send(users);
 });
 
 router.post('/getusers', function(req, res) {
 	// req object will have form: {name(string), profileImg(string URL), bgColor(string), interests(string) }
 	var newUser = req.body;
-	newUser.id = users.length;
 	users.push(newUser);
 	res.end('{"success" : "Updated Successfully", "status" : 200}');
 });
