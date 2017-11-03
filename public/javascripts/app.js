01418a63bf6b611c5f1edf0b28df27223352dc0d
caussepa <caussepa@gmail.com>
2017-11-03 00:12:07 +0000

    stuff

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,25 @@
 // javacript file for app
 
-var app = angular.module('app', ['ui.router']);
+angular.module('app', ['ui.router'])
 
-app.config([
+.factory('userList', [
+        '$http',
+        function ($http) {
+            var API_ROOT = 'getusers';
+            console.log("in userList");
+            return {
+                    get: function() {
+                            return $http
+                            .get(API_ROOT)
+                            .then(function (resp) {
+                                    console.log(resp.data);
+                                    return resp.data;
+                            })
+                    }
+            };
+       }
+])
+.config([
     '$stateProvider',
     '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
@@ -10,59 +27,55 @@ app.config([
 	    .state('home', {
 	        url: '/home',
 	        templateUrl: '/home.html',
-	        controller: 'mainCtrl' })
-	    .state('user', {
-		    url: '/user/{id}',
+	        controller: 'mainCtrl' 
+	    })
+	    .state('users', {
+		    url: '/users/{id}',
 		    templateUrl: '/user.html',
 		    controller: 'pageCtrl'
 	    });
 	    $urlRouterProvider.otherwise('home');
+//            $qProvider.errorOnUnhandledRejections(false);
 	}
-])
-.factory('userList', userList)
-.controller('mainCtrl', mainCtrl)
-.controller('pageCtrl', pageCtrl);
 
-function userList($http) {
-	var API_ROOT = 'getusers';
-	console.log("in userList");
-	return {
-		get: function() {
-			return $http
-			.get(API_ROOT)
-			.then(function (resp) {
-				console.log(resp.data);
-				return resp.data;
-			})
-		}
-	};
-}
-function mainCtrl($scope, userList, $http) {
-	$scope.users = [];
-	console.log("in mainCtrl");
-	getUsers();
-	
-	function getUsers() {
-			userList.get()
-		.then(function (data) {
-			$scope.users = data;
-		});
-	}
-	$scope.createUser = function() {
-		var formData = {name:$scope.name, bgColor:$scope.bgColor, profileImg:$scope.profileImg, interests:$scope.interests};
-		$http({
-			url: 'getusers',
-			method: 'POST',
-			data: formData
-		}).then(function(data, status) {
-			console.log("User created");
-			getUsers();
-		}, 
-		[function(data, status) {
-			console.log("Failed to create");
-		}]);
-	};
-}
-function pageCtrl($scope, userList) {
-	//
-}
+])
+.controller('mainCtrl', [
+	'$scope',
+	'userList',
+	'$http',
+	function($scope, userList, $http) {
+            $scope.users = userList.users;
+            console.log("in mainCtrl");
+            getUsers();
+
+            function getUsers() {
+           	      userList.get()
+                .then(function (data) {
+                        $scope.users = data;
+                });
+            }
+            $scope.createUser = function() {
+                var formData = {name:$scope.name, bgColor:$scope.bgColor, profileImg:$scope.profileImg, interests:$scope.interests};
+                $http({
+                        url: 'getusers',
+                        method: 'POST',
+                        data: formData
+                }).then(function(data, status) {
+                        console.log("User created");
+                        getUsers();
+                },
+                [function(data, status) {
+                        console.log("Failed to create");
+                }]);
+            };
+        }])
+
+.controller('pageCtrl',[
+	'$scope',
+	'userList',
+	'$stateParams',
+	function($scope, userList,$stateParams) {
+            $scope.users = userList.users[$stateParams.id];
+
+}]);
+
